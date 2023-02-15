b7a50d7fadfb39ea4943295e314493dd5ec361c3
John Tetreault <46641218+mytechguyri@users.noreply.github.com>
2023-02-15 00:48:17 -0500

    Update dapnetnotifier.py

--- dapnetnotifier.py
+++ dapnetnotifier.py
@@ -1,78 +1,94 @@
-#################################################################################
-
-# DAPNET Notifier
-# Developed by: Jeff Lehman, N8ACL
-# Current Version: 09132022
-# https://github.com/n8acl/DAPNETNotifier
-
-# Questions? Comments? Suggestions? Contact me one of the following ways:
-# E-mail: n8acl@qsl.net
-# Twitter: @n8acl
-# Discord: Ravendos#7364
-# Mastodon: @n8acl@mastodon.radio
-# Website: https://www.qsl.net/n8acl
-# Blog: https://n8acl.github.io
-
-###################   DO NOT CHANGE BELOW   #########################
-
-#############################
-##### Import Libraries
-import config as cfg
+#!/usr/bin/python3
+#Special thanks to KR0SIV for his DAPNET2APRS program https://github.com/KR0SIV/DAPNET2APRS
+#and N8ACL for his DAPNETNotifier program https://github.com/n8acl/DAPNETNotifier  
+#on both of which this program is based
+#Copyright 2023 John Tetreault WA1OKB, released under the Gnu General Public License v3.0
+#
 import json
 import re
 import requests
 import time
 import http.client, urllib
-import os
 import sys
-import sqlite3 as sql
+import aprslib
 from requests.auth import HTTPBasicAuth
 from os import system, name
 from time import sleep
-from sqlite3 import Error
 
-# libary only needed if Discord is configured in config.py
-if cfg.discord:
-    from discord_webhook import DiscordWebhook, DiscordEmbed
 
-# libraries only needed if Telegram is configured in config.py
-if cfg.telegram:
-    import telegram
-
-#############################
-##### Define Variables
+##### Define Configs
 first_run = True
 linefeed = "\r\n"
-dapnet_url = 'http://www.hampager.de:8080/calls?ownerName=' + cfg.dapnet_username
-database = os.path.dirname(os.path.abspath(__file__)) +  "/dapnet.db"
+wait_time = 60
+dapnet_username = 'YOUR_DAPNET_LOGIN'
+dapnet_password = 'YOUR_DAPNET_API_PASSWORD'
+dapnet_url = 'http://www.hampager.de:8080/calls?ownerName=' +dapnet_username
+callsign = 'YOUR_CALLSIGN'
+aprs_passcode = 'YOUR_APRS_PASSCODE'
+pager_id = 'YOUR_DAPNET_RIC'
+send_to = 'APRS_CALLSIGN_TO_SEND_TO'
+db_engine = 'mysql'     #you can use either 'mysql' or 'sqlite', I didnt want sqlite hammering away at my Pi SD card, so I used my MariaDB server on my nas instead
 
-#############################
-##### Define Functions
+if db_engine == 'mysql':
+  import mysql.connector
+  mysqlhost = "YOUR_MYSQL_SERVER"             #If you want to use MySQL/MariaDB, put your server info here
+  mysqluser = "MYSQL_SERVER_LOGIN"
+  mysqlpassword = "MYSQL_SERVER_PASSWORD"
+  db = "dapnet"
+else:
+  import sqlite3 as sql
+  from sqlite3 import Error
+  import os
+  db = os.path.dirname(os.path.abspath(__file__)) + "/dapnet.db"
+
 
 ##### Define SQL Functions
 def create_connection(db_file):
-    # Creates connection to dapnet.db SQLlite3 Database
     conn = None
-    try:
-        conn = sql.connect(db_file)
-    except Error as e:
-        print(e)
+    if db_engine == 'mysql':
+    #Create connection to MySQL/MariaDB Database    
+        try:
+             conn = mysql.connector.connect(
+               host = mysqlhost,
+               user = mysqluser,
+               password = mysqlpassword,
+               database = db_file
+             )
+        except Error as e:
+             print (e)
+    else:
+    # Creates connection to dapnet.db SQLlite3 Database
+       try:
+           conn = sql.connect(db_file)
+       except Error as e:
+           print (e)
     return conn
 
 def exec_sql(conn,sql):
     # Executes SQL for Updates, inserts and deletes
-    cur = conn.cursor()
+    cur = conn.cursor(buffered=True)
     cur.execute(sql)
     conn.commit()
 
 def select_sql(conn,sql):
     # Executes SQL for Selects
-    cur = conn.cursor()
+    cur = conn.cursor(buffered=True)
     cur.execute(sql)
     return cur.fetchall()
 
 def new(conn):
 # Create new database if not exists
+    if db_engine == 'mysql':
+      conn = mysql.connector.connect(
+          host = mysqlhost,
+          user = mysqluser,
+          password = mysqlpassword
+      )
+      cur = conn.cursor(buffered=True)
+      sql = "CREATE DATABASE " + db
+      cur.execute(sql)
+      sql = "USE " + db
+      cur.execute(sql)
 
     create_message_table = """ create table if not exists messages (
 text text, 
@@ -93,50 +109,48 @@ timestamp text
         exec_sql(conn, sql)
 
 
+
 ##### Get API Data
 def get_api_data():
-    return requests.get(dapnet_url, auth=HTTPBasicAuth(cfg.dapnet_username,cfg.dapnet_password)).json()
+    return requests.get(dapnet_url, auth=HTTPBasicAuth(dapnet_username,dapnet_password)).json()
 
-##### Define Service Functions
-def send_discord(msg):
-    webhook = DiscordWebhook(url=cfg.discord_wh)
-
-    embed = DiscordEmbed(title="New DAPNET Message", description=msg)
-    webhook.add_embed(embed)
-
-    response = webhook.execute() 
-
-def send_pushover(msg):
-    connn = http.client.HTTPSConnection("api.pushover.net:443")
-    connn.request("POST", "/1/messages.json",
-        urllib.parse.urlencode({
-        "token": cfg.pushover_token,
-        "user": cfg.pushover_userkey,
-        "message": msg,
-        }), { "Content-type": "application/x-www-form-urlencoded" })
-    connn.getresponse()
-
-def post_to_webhook(msg, wh_url):
-    # used for Slack and Mattermost
-    response = requests.post(
-        wh_url, data=json.dumps(msg),
-        headers={'Content-Type': 'application/json'}
-    )
-
-def send_telegram(msg):
-    bot = telegram.Bot(token=cfg.telegram_bot_token)
-    bot.sendMessage(chat_id=cfg.telegram_chat_id, text=msg)
+##### Send APRS function
+def send_aprs(msg):
+        try:
+              print("Forwarding for " + pager_id + " to APRS: " + msg)
+              AIS = aprslib.IS(callsign, aprs_passcode, port=14580)
+              AIS.connect()
+              AIS.sendall("DAPNET>APRS,TCPIP*::" + send_to.ljust(9) + ":" + msg)
+        except:
+            pass
 
 
-#############################
 ##### Main Program
 
 # check to see if the database exists. If not create it. Otherwise create a connection to it for the rest of the script
-if not os.path.exists(database):
-    conn = create_connection(database)
-    new(conn)
+if db_engine == 'mysql':
+     conn = mysql.connector.connect(
+          host = mysqlhost,
+          user = mysqluser,
+          password = mysqlpassword
+     )
+     cur = conn.cursor(buffered=True)
+     cur.execute("SHOW DATABASES")
+     db_exists = 0
+     for x in cur:
+        if str(x) == "('"+db+"',)":
+           sql = "USE " + db
+           cur.execute(sql)
+           db_exists = 1
+     if db_exists == 0:
+        new(conn)
 else:
-    conn = create_connection(database) 
+  conn = sql_connection(db)
+  if not os.path.exists(db):
+    conn = sql_connection(db)
+    new(conn)
+  else:
+    conn = sql_connection(db) 
 
 # Check API and if the last message was not already sent, send it... else ignore it.
 try:
@@ -145,10 +159,10 @@ try:
             first_run = False
         else:
             # Wait the check time to not pound the API and get rate Limited
-            if cfg.wait_time < 60:
+            if wait_time < 60:
                 sleep(60)
             else:
-                sleep(cfg.wait_time) 
+                sleep(wait_time) 
 
             # get the data from the API
             data = get_api_data()
@@ -171,18 +185,11 @@ try:
                     exec_sql(conn,sql)
                     
                     # Send the message 
-                    if cfg.discord:
-                        send_discord(text)
-                    if cfg.telegram:
-                        send_telegram(text)
-                    if cfg.mattermost:
-                        post_to_webhook({'text': text}, cfg.mattermost_wh)
-                    if cfg.slack:
-                        post_to_webhook({'text': text}, cfg.slack_wh)
-                    if cfg.pushover:
-                        send_pushover(text)
+                    send_aprs(text)
+                    
 
                     break
 
 except Exception as e:
     print(str(e))
+
